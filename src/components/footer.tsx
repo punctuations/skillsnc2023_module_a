93b9ce1c11a958a0771306baeb010c7a0e40f6e0
matt <matt@dont-ping.me>
2023-05-26 15:51:59 -0500

    init

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,15 +3,15 @@ export const Footer = () => {
 		<>
 			<hr className="border-1 mx-6 h-2 w-full border-gray-200" />
 
-			<footer className="space-x mx-6 mb-32 flex w-full flex-row items-center justify-around">
-				<div className="flex flex-col items-start">
+			<footer className="space-x mx-6 mb-32 grid w-full place-content-center gap-x-6 sm:grid-cols-1 md:grid-cols-2">
+				<div className="flex flex-col items-center">
 					<header className="text-xl font-bold">Contact Deatils</header>
 					<p>Winnepeg Tourism Office</p>
 					<p>PO Box 100</p>
 					<p>Winnepeg, MB</p>
 					<p>A1A 1A1</p>
 				</div>
-				<div>
+				<div className="flex flex-col items-center">
 					<header className="text-xl font-bold">Socials</header>
 					<p>
 						Instagram:{' '}
